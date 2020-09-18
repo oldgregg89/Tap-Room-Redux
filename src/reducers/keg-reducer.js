b2cb412fd3aaa6d2752b2ef6565001aa2b0b3f06
Ian Gregg <iangregg188@gmail.com>
2020-09-18 10:28:47 -0700

    finishing out reducers for Add keg.

--- src/reducers/keg-reducer.js
+++ src/reducers/keg-reducer.js
@@ -1,6 +1,17 @@
 export default (state = {}, action) => {
   const {name, abv, type, price, brew}
   switch (action.type) {
-  case 
+  case 'ADD_KEG':
+    return Object.assign({},state, {
+      [id]: {
+        name: name,
+        abv: abv,
+        type: type,
+        price: price,
+        brew: brew,
+      }
+    });
+  default:
+    return state;
   }
 };
