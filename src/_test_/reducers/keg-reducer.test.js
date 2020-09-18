7c2f20816f0116fa4ad0f7fefb7152643734da49
Ian Gregg <iangregg188@gmail.com>
2020-09-18 11:52:21 -0700

    working on testing reducers.

--- src/_test_/reducers/keg-reducer.test.js
+++ src/_test_/reducers/keg-reducer.test.js
@@ -1,1 +1,14 @@
 import kegReducer from '../../reducers/keg-reducer';
+
+describe('kegReducer', () => {
+
+  let action;
+  const kegData = {
+    names: 'A bird',
+    abv: '12',
+    type: 'IPA',
+    price: '2.99',
+    brewer: 'westcoast'
+    id: 1
+
+  };
--- src/reducers/keg-reducer.js
+++ src/reducers/keg-reducer.js
@@ -11,6 +11,10 @@ export default (state = {}, action) => {
         brew: brew,
       }
     });
+  case 'DELETE_KEG':
+    const newState = { ...state };
+    delete newState[id];
+    return newState;
   default:
     return state;
   }
