025f7d2084914334c5dac0da91760ec79bfd9a47
Ian Gregg <iangregg188@gmail.com>
2020-09-18 12:35:55 -0700

    working on the testing portion of the application.

--- src/_test_/reducers/keg-reducer.test.js
+++ src/_test_/reducers/keg-reducer.test.js
@@ -4,12 +4,27 @@ describe('kegReducer', () => {
 
   let action;
   const kegData = {
-    names: 'A bird',
+    name: 'A witches brew',
     abv: '12',
     type: 'IPA',
     price: '2.99',
     brewer: 'westcoast',
     id: 1
-
   };
+
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(kegReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new mech data to KegList', () => {
+    const { name, abv, type, price, brewer, id } = kegData;
+    action = {
+      type: 'ADD_POST',
+      name: name,
+      abv: abv,
+      type: type,
+      price: price,
+      brewer: brewer,
+      id: id
+    };
 }
